e53319f4634c968b0e722107ce30568af8884e56
hawaiiH <vladgusakov@gmail.com>
2020-10-28 19:23:15 +0200

    Updates

---
+++ precache-manifest.c0a4e18e8f61e6b33c35d125409505a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54120db02a0aa82aef2eca65f2604136",
+    "url": "/udemy_react-redux_resto-build/index.html"
+  },
+  {
+    "revision": "4b45966e317b4622f553",
+    "url": "/udemy_react-redux_resto-build/static/css/main.205a1715.chunk.css"
+  },
+  {
+    "revision": "214187ecffdbaaf596d7",
+    "url": "/udemy_react-redux_resto-build/static/js/2.3e574817.chunk.js"
+  },
+  {
+    "revision": "4b45966e317b4622f553",
+    "url": "/udemy_react-redux_resto-build/static/js/main.22dafadb.chunk.js"
+  },
+  {
+    "revision": "ac4e6f9580f1d3280c47",
+    "url": "/udemy_react-redux_resto-build/static/js/runtime~main.5e9e9466.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/udemy_react-redux_resto-build/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/udemy_react-redux_resto-build/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
